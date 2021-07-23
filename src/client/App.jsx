c73045849a5899a3a87d085e9a744165106bedab
Dennis <DennisLoska@users.noreply.github.com>
2021-07-23 03:08:41 +0200

    🚧 add: wip page

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import './App.scss';
 
 const App = () => {
   // setup initial component state
   const [data, setData] = useState(null);
   return (
-    <div>
-      Hello world
+    <div className="colorBackdrop">
+      <div className="titleContainer">
+        <div className="titleContainer__title">Re:Faith</div>
+        <div className="titleContainer__subtitle">Seite wird aktuell bearbeitet!</div>
+      </div>
     </div>
   );
 };
