420732af8653e80da054bb083784538b3121b3d3
Dennis <DennisLoska@users.noreply.github.com>
2022-06-07 15:58:13 +0200

    ✨ feat: Poetry container

--- src/client/containers/Poetry.jsx
+++ src/client/containers/Poetry.jsx
@@ -1,19 +1,30 @@
 
 import React from 'react';
+import Markdown from 'markdown-to-jsx';
 import CardWrapper from '../components/card/CardWrapper';
 import Card from '../components/card/Card';
 import useContainer from '../hooks/useContainer';
 
+const importAll = r => r.keys().map(r);
+
+// import all poem markdown files dynamically
+const poems = importAll(require.context('../content/poems', false, /\.(md)$/));
+
 const Poetry = () => useContainer(
   <CardWrapper>
-    <Card>✏️ ...</Card>
-    <Card>✏️ ...</Card>
-    <Card>✏️ ...</Card>
-    <Card>✏️ ...</Card>
-    <Card>✏️ ...</Card>
-    <Card>✏️ ...</Card>
-    <Card>✏️ ...</Card>
-    <Card>✏️ ...</Card>
+    {
+      Array.isArray(poems) ? (
+        poems.map(poem => (
+          <Card
+            width="35em"
+            height="fit-content"
+            key={new Date().getTime() + Math.floor(Math.random() * 42)}
+            isPoem
+          >
+            <Markdown className="poem">{poem?.default}</Markdown>
+          </Card>
+        ))) : null
+    }
   </CardWrapper>
 );
 
