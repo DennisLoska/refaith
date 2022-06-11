dac8b9837cd93dc850220a29dac6b581e091a097
Dennis <DennisLoska@users.noreply.github.com>
2022-06-11 04:18:52 +0200

    🚸 feat: intelligent music player

--- src/client/containers/Music.jsx
+++ src/client/containers/Music.jsx
@@ -1,57 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactJkMusicPlayer from 'react-jinke-music-player';
 import config from '../config/config';
 import useContainer from '../hooks/useContainer';
-import TitleWrapper from '../components/construction/TitleWrapper';
-import TitleText from '../components/construction/TitleText';
 import AlbumCover from '../assets/images/zeitkapsel.png';
 import CardWrapper from '../components/card/CardWrapper';
+import Card from '../components/card/Card';
 
 const Music = () => useContainer(
-  <>
-    <CardWrapper>
-      <TitleWrapper>
-        <a
-          href={config?.playlist}
-          target="_blank"
-          rel="noopener noreferrer"
-          width="20%"
-        >
-          <img
-            src={AlbumCover}
-            width="100%"
-            alt="Zeitkapsel Albumcover"
-            style={{ marginBottom: '50px', borderRadius: '5px' }}
-          />
-        </a>
-        <TitleText size="1.5em">
-          Zeitkapsel ist ein persönliches Projekt aus dem Jahr 2021,
-          {' '}
-          <br />
-          {' '}
-          um Frieden mit meiner Vergangenheit zu schließen!
-        </TitleText>
-      </TitleWrapper>
-    </CardWrapper>
-    <ReactJkMusicPlayer
-      theme="dark"
-      bounds="html"
-      mode="full"
-      glassBg
-      spaceBar
-      responsive
-      showDownload
-      showThemeSwitch
-      showMediaSession
-      autoPlay={false}
-      remove={false}
-      showLyric={false}
-      toggleMode={false}
-      autoPlayInitLoadPlayList={false}
-      defaultVolume={0.7}
-      audioLists={config?.audioList}
-    />
-  </>
+  (() => {
+    const [album, setAlbum] = useState(null);
+
+    return (
+      <>
+        <CardWrapper>
+          <Card
+            width="20em"
+            height="fit-content"
+            onClick={() => setAlbum('zeitkapsel')}
+            padding="0px"
+          >
+            <div className="albumCover">
+              <img
+                src={AlbumCover}
+                width="100%"
+                alt="Zeitkapsel Albumcover"
+                style={{ marginBottom: '0px', borderRadius: '0px' }}
+              />
+            </div>
+            <div className="album">
+              <div>
+                <h2>Zeitkapsel (2021)</h2>
+                <p>
+                  <a href={config?.playlist} target="_blank" rel="noopener noreferrer">Zeitkapsel </a>
+                  ist ein persönliches Projekt aus dem Jahr 2021,
+                  um Frieden mit unterschiedlichsten Phasen aus meiner Vergangenheit zu schließen.
+                </p>
+                <p>
+                  Die Beats stammen von diversen Produzenten (siehe entsprechende Videobeschreibungen).
+                  Das Mixing, Mastering und Recording hat Norman von
+                  {' '}
+                  <a href="https://www.cuttel.de/" target="_blank" rel="noopener noreferrer">Cuttel Studio</a>
+                  {' '}
+                  übernommen. Vielen Dank für die gute Zusammenarbeit mein Bester!
+                </p>
+              </div>
+            </div>
+          </Card>
+        </CardWrapper>
+        {
+          album !== null ? (
+            <ReactJkMusicPlayer
+              theme="dark"
+              mode="full"
+              glassBg
+              spaceBar
+              responsive
+              showDownload
+              showThemeSwitch={false}
+              showMediaSession
+              autoPlay={false}
+              remove={false}
+              showLyric={false}
+              toggleMode={false}
+              audioLists={config?.audioList[album]}
+            />
+          ) : null
+        }
+      </>
+    );
+  })()
 );
 
 export default Music;
