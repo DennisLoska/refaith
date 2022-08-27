fd98641de0b194fc1b4230203802dbf91de6acc8
Dennis <DennisLoska@users.noreply.github.com>
2022-08-27 10:24:18 +0200

    ⚡️ remove: cache contorl after evaluation

--- src/server/index.js
+++ src/server/index.js
@@ -3,10 +3,5 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static('dist', {
-  cacheControl: true,
-  setHeaders(res) {
-    res.set('Cache-Control', 'max-age=31536000');
-  }
-}));
+app.use(express.static('dist'));
 app.listen(port, () => console.log(`Listening on port ${port || 5000}!`));
