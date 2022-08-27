1d8f658bb6e1db8ee85188b791e72425455ec9a6
Dennis <DennisLoska@users.noreply.github.com>
2022-08-27 10:03:14 +0200

    ⚡️ add: caching

--- src/server/index.js
+++ src/server/index.js
@@ -3,5 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.static('dist'));
+app.use(express.static('dist', {
+  cacheControl: true,
+  setHeaders(res) {
+    res.set('Cache-Control', 'max-age=31536000');
+  }
+}));
 app.listen(port, () => console.log(`Listening on port ${port || 5000}!`));
