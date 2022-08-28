3029161aab777a63b69e984f15c599d403b8ea88
Dennis <DennisLoska@users.noreply.github.com>
2022-08-28 12:54:35 +0200

    ✨ add: simple logging middleware

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const pino = require('pino');
+const expressPino = require('express-pino-logger');
+
+const logger = pino({ level: process.env.LOG_LEVEL || 'info' });
+const expressLogger = expressPino({ logger });
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(expressLogger);
+
+app.use((req, res, next) => {
+  logger.info(req);
+  next();
+});
+
 app.use(express.static('dist'));
-app.listen(port, () => console.log(`Listening on port ${port || 5000}!`));
+app.listen(port, () => logger.info(`Server running on port ${port || 5000}!`));
