6790edb2ad52f314b68836eb87cd64fc8a6b2a5a
Dennis Loska <DennisLoska@users.noreply.github.com>
2023-06-29 21:34:30 +0200

    ✨ feat: poetry window loader

--- src/client/containers/Poetry.jsx
+++ src/client/containers/Poetry.jsx
@@ -1,49 +1,13 @@
 import React, { useMemo } from 'react';
 import Markdown from 'markdown-to-jsx';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+import { Virtuoso } from 'react-virtuoso';
 import CardWrapper from '../components/card/CardWrapper';
 import Card from '../components/card/Card';
 import useContainer from '../hooks/useContainer';
 import BackgroundImage from '../assets/images/optimized/library.webp';
 
 const importAll = r => r.keys().map(r);
-
-const createPoems = (poems, illustrations) => poems.map((poem, i) => (
-  window.innerWidth <= 1024 ? (
-    <Card
-      height="fit-content"
-      isPoem
-      style={{ cursor: 'pointer' }}
-      padding="0 0 25px 0"
-      key={`poem-${i}`}
-    >
-      <div style={{ height: '340px', overflow: 'hidden' }}>
-        <LazyLoadImage
-          alt="Poem illustration"
-          src={illustrations[i < illustrations.length ? i : Math.ceil(i * 0.5)]}
-          width="100%"
-        />
-      </div>
-      <Markdown className="poem">{poem?.default}</Markdown>
-    </Card>
-  ) : (
-    <Card
-      height="fit-content"
-      isPoem
-      padding="0 0 25px 0"
-      key={`poem-${i}`}
-    >
-      <div style={{ height: 'fit-content', overflow: 'hidden' }}>
-        <LazyLoadImage
-          alt="Poem illustration"
-          src={illustrations[i < illustrations.length ? i : Math.ceil(i * 0.5)]}
-          width="100%"
-        />
-      </div>
-      <Markdown className="poem">{poem?.default}</Markdown>
-    </Card>
-  )
-));
 
 const Poetry = () => {
   const poems = useMemo(() => importAll(
@@ -54,14 +18,64 @@ const Poetry = () => {
     require.context('../assets/images/poems/optimized', false, /\.(webp)$/)
   ).sort(() => Math.random() - 0.5));
 
-  const poemList = useMemo(
-    () => createPoems(poems, illustrations),
-    [poems, illustrations]
-  );
+  const Poem = useMemo(() => ({ i }) => (
+    window.innerWidth <= 1024 ? (
+      <Card
+        height="fit-content"
+        isPoem
+        padding="0 0 25px 0"
+        margin="0px auto 50px"
+        key={`poem-${i}`}
+      >
+        <div style={{ height: '340px', overflow: 'hidden' }}>
+          <LazyLoadImage
+            alt="Poem illustration"
+            src={illustrations[i < illustrations.length ? i : Math.ceil(i * 0.5)]}
+            width="100%"
+          />
+        </div>
+        <Markdown className="poem">{poems[i]?.default}</Markdown>
+      </Card>
+    ) : (
+      <Card
+        height="fit-content"
+        isPoem
+        padding="0 0 25px 0"
+        margin="0px auto 50px"
+        key={`poem-${i}`}
+      >
+        <div style={{ height: 'fit-content', overflow: 'hidden' }}>
+          <LazyLoadImage
+            alt="Poem illustration"
+            src={illustrations[i < illustrations.length ? i : Math.ceil(i * 0.5)]}
+            width="100%"
+          />
+        </div>
+        <Markdown className="poem">{poems[i]?.default}</Markdown>
+      </Card>
+    )
+  ));
 
+  // {Array.isArray(poemList) && poemList}
   return useContainer(
-    <CardWrapper padding="25px 6.8%">
-      {Array.isArray(poemList) && poemList}
+    <CardWrapper padding="50px 0px">
+      {
+        Array.isArray(poems) && Array.isArray(illustrations) && (
+          <>
+            <Virtuoso
+              style={{
+                height: '75vh',
+                width: '100%',
+                overflowX: 'hidden'
+              }}
+              totalCount={poems.length}
+              itemContent={index => (
+                <Poem i={index} />
+              )}
+            />
+          </>
+        )
+      }
     </CardWrapper>,
     BackgroundImage
   );
